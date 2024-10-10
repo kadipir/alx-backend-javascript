3cc7e5e0a79688ed7066859f8f6780b9add87393
truth <trutherkadi@gmail.com>
2024-10-10 15:08:14 -0700

    js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,9 @@ export default function updateUniqueItems(map){
 	if (!map instanceof Map){
 		throw new Error('Cannot process');
 	}
-	map.forEach((key, value) => {
-		if (value == 1){
-			map.set(key = 100);
+	map.forEach((value, key) => {
+		if (value === 1){
+			map.set(key, 100);
 		}
 	});
 }
